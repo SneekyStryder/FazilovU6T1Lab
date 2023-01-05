01c408b4f85b0e291e2f35d0f84d27dedcec3058
Imran Fazilov <ifazilov3584@bths.edu>
2023-01-05 12:28:36 -0500

    period end

--- src/RandomRunner.java
+++ src/RandomRunner.java
@@ -1,2 +1,57 @@
-package PACKAGE_NAME;public class RandomRunner {
+public class RandomRunner {
+    public static void main(String[] args) {
+
+        // 1
+        System.out.println("\nQuestion 1");
+        int[] heights = new int[3];
+        for (int i = 0; i < 3; i++) {
+            System.out.println(heights[i]);
+        }
+        heights[0] = 78;
+        heights[1] = 72;
+        heights[2] = 69;
+        for (int i = 0; i < 3; i++) {
+            System.out.println(heights[i]);
+        }
+
+        // 2
+        System.out.println("\nQuestion 2");
+        boolean[] bools = {true, false, true, false, true, false};
+        bools[0] = false;
+        System.out.println(bools.length + "\n" + bools[0] + "\n" + bools[5]);
+
+        // 3
+        System.out.println("\nQuestion 3");
+        String[] alphabeticalNames = {"Abby", "David", "Charlie", "Lauren"};
+        String temp;
+        temp = alphabeticalNames[1];
+        alphabeticalNames[1] = alphabeticalNames[2];
+        alphabeticalNames[2] = temp;
+        System.out.println(alphabeticalNames[0]);
+        System.out.println(alphabeticalNames[1]);
+        System.out.println(alphabeticalNames[2]);
+        System.out.println(alphabeticalNames[3]);
+
+        // 4
+        System.out.println("\nQuestion 4");
+        double[] array1 = {7.5, 10.0};
+        double[] array2 = {8.2, 14.8};
+        double[] array3 = {array1[0] + array2[0], array1[1] * array2[1]};
+        System.out.println(array3[0]);
+        System.out.println(array3[1]);
+
+        // 5
+        System.out.println("\nQuestion 5");
+        String[] animals = new String[4];
+        for (int i = 0; i < 4; i++) {
+            System.out.println(animals[i]);
+        }
+
+        // 6
+        System.out.println("\nQuestion 6");
+        Dog dog1 = new Dog("Sparky", 4);
+        Dog dog2 = new Dog("Toby", 7);
+        Dog dog3 = new Dog("Fiona", 12);
+
+    }
 }
