4c5ce1ec3e0f60d5080989ae1e7cb1135500ddb2
Sneeky Stryder <ifazilov3584@bths.edu>
2023-01-05 19:53:31 -0500

    ye

--- src/RandomRunner.java
+++ src/RandomRunner.java
@@ -46,12 +46,26 @@ public class RandomRunner {
         for (int i = 0; i < 4; i++) {
             System.out.println(animals[i]);
         }
+        animals[0] = "dog";
+        animals[1] = "camel";
+        animals[2] = "aardvark";
+        animals[3] = "bunny";
+
+        animals[2] += "s";
+        System.out.println(animals[2]);
+        System.out.println(animals.length);
+        System.out.println(animals[2].length());
 
         // 6
         System.out.println("\nQuestion 6");
         Dog dog1 = new Dog("Sparky", 4);
         Dog dog2 = new Dog("Toby", 7);
         Dog dog3 = new Dog("Fiona", 12);
+
+        Dog[] dogs = {dog1, dog2, dog3};
+        System.out.println(dogs[0].getName());
+        System.out.println(dogs[1].getName());
+        System.out.println(dogs[2].getName());
 
     }
 }
